faa4a1ce5e6a1f319cc5fc4acdb6367f6a8a377a
raunak jasrasaria <raunakjasrasaria04@gmail.com>
2021-05-04 23:14:57 +0530

    Database linked

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-var items = ["first","second"];
-var workItems = [];
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = new mongoose.Schema({
+  name: String
+});
+
+const Item = mongoose.model("Item",itemsSchema);
+
+const item1 = new Item({
+  name:"Welcome to your todoList"
+});
+const item2 = new Item({
+  name:"Hit the + button to add a new item"
+});
+const item3 = new Item({
+  name:"<-- Hit this to delete an item"
+});
+
+Item.insertMany([item1, item2, item3],function(err){
+  if(err){
+    console.log(err);
+  }else{
+    console.log("Successfully added default items to DB");
+  }
+})
+
 
 app.get("/", function(req, res) {
 
